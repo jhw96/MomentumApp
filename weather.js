0aabd255f1a72f246aeb009a5fe7abf2b582baa1
장현웅 <ds6tvi@naver.com>
2021-01-11 20:10:09 +0900

    Get and Show WeatherInfo at Current Location

---
+++ weather.js
@@ -0,0 +1,55 @@
+const weatherContainer = document.querySelector('.js-weather');
+
+const API_KEY = "d467ff8960f638eee54f891435a6f22a";
+const COORDS = 'coords';
+
+
+function getWeather(latitude, longitude) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    .then(function(response) { // then : Function do work After Recieving data
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weatherContainer.innerText = `${temperature} °C at ${place}`;
+    })
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude: latitude,
+        longitude: longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+    console.log("Can't access geo location.");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
